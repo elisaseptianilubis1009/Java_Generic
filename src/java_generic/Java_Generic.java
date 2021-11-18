4ee82823523e51fea279d7e1083c0d8cc6e104ee
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-11-18 16:04:34 +0700

    git ini Java_Generic

---
+++ src/java_generic/Java_Generic.java
@@ -0,0 +1,14 @@
+
+package java_generic;
+
+
+public class Java_Generic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+    
+}
