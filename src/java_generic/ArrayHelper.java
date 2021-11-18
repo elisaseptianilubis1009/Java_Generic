b30828b74f159e43348343faf5588b47e9a8ed00
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-11-18 16:36:24 +0700

    Implement Generic Method

---
+++ src/java_generic/ArrayHelper.java
@@ -0,0 +1,11 @@
+
+package java_generic;
+
+
+public class ArrayHelper {
+    
+    public static <T> int count(T[] array){
+        return array.length;
+    }
+    
+}
--- src/java_generic/Java_Generic.java
+++ src/java_generic/Java_Generic.java
@@ -5,16 +5,24 @@ package java_generic;
 public class Java_Generic {
 
     public static void main(String[] args) {
-        Generic<String, Integer> stringGeneric= new Generic<String, Integer>("Julia", 21);
-        
-        System.out.println("Nama :"+stringGeneric.getName());
-        System.out.println("Umur :"+stringGeneric.getName());
-        
-        stringGeneric.setName("Elisa Septiani Lubis");
-        stringGeneric.setUmur(22);
-        
-        System.out.println("Nama :"+stringGeneric.getName());
-        System.out.println("Umur :"+ stringGeneric.getUmur());
+//============================ Generic Class================================
+//        Generic<String, Integer> stringGeneric= new Generic<String, Integer>("Julia", 21);
+//        
+//        System.out.println("Nama :"+stringGeneric.getName());
+//        System.out.println("Umur :"+stringGeneric.getName());
+//        
+//        stringGeneric.setName("Elisa Septiani Lubis");
+//        stringGeneric.setUmur(22);
+//        
+//        System.out.println("Nama :"+stringGeneric.getName());
+//        System.out.println("Umur :"+ stringGeneric.getUmur());
+
+//========================= Generic Method =================================
+    String [] stringArray={"Elisa","Septiani","Lubis"};
+    Integer[] integerArray={3,4,5,7};
+    
+        System.out.println(ArrayHelper.<String>count(stringArray));
+        System.out.println(ArrayHelper.count(integerArray));
     }
     
 }
